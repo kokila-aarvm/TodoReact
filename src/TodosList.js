bcdd78f49fea04aa4c3aede739c3d2b542acfcfb
Kokila <kokila.k@aarvm.com>
2023-02-15 12:55:52 +0530

    changes added

--- src/TodosList.js
+++ src/TodosList.js
@@ -1,3 +1,4 @@
+// In new branch .... checking
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
@@ -14,7 +15,7 @@ const fetchTodos = () => async (dispatch) => {
 const TodosList = () => {
   const dispatch = useDispatch();
   const todos = useSelector((state) => state);
-
+  const [addInput, setAddInput] = useState("");
   const [editTodo, setEditTodo] = useState(null);
 
   useEffect(() => {
@@ -28,44 +29,67 @@ const TodosList = () => {
   const updateTodo = (id, title) => (dispatch) => {
     dispatch({ type: "UPDATE_TODO", payload: { id, title } });
   };
+  const addTodo = (addInput) => (dispatch) => {
+    dispatch({
+      type: "ADD_TODOS",
+      payload: { id: Math.random(), title: addInput },
+    });
+  };
+  console.log(todos);
 
   return (
-    <ol>
-      {todos.map((todo) => (
-        <li key={todo.id}>
-          {editTodo && editTodo.id === todo.id ? (
-            <>
-              <input
-                type="text"
-                value={editTodo.title}
-                onChange={(e) =>
-                  setEditTodo({ ...editTodo, title: e.target.value })
-                }
-              />
-              <button
-                onClick={() => {
-                  dispatch(updateTodo(todo.id, editTodo.title));
-                  setEditTodo(null);
-                }}
-              >
-                Update
-              </button>
-              <button onClick={() => dispatch(deleteTodo(todo.id))}>
-                Delete
-              </button>
-            </>
-          ) : (
-            <>
-              {todo.title}
-              <button onClick={() => setEditTodo(todo)}>Edit</button>
-              <button onClick={() => dispatch(deleteTodo(todo.id))}>
-                Delete
-              </button>
-            </>
-          )}
-        </li>
-      ))}
-    </ol>
+    <div>
+      <h1>Redux Todo</h1>
+      <input
+        type="text"
+        value={addInput}
+        onChange={(e) => setAddInput(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          dispatch(addTodo(addInput));
+          setAddInput("");
+        }}
+      >
+        Add
+      </button>
+      <ol>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {editTodo && editTodo.id === todo.id ? (
+              <>
+                <input
+                  type="text"
+                  value={editTodo.title}
+                  onChange={(e) =>
+                    setEditTodo({ ...editTodo, title: e.target.value })
+                  }
+                />
+                <button
+                  onClick={() => {
+                    dispatch(updateTodo(todo.id, editTodo.title));
+                    setEditTodo(null);
+                  }}
+                >
+                  Update
+                </button>
+                <button onClick={() => dispatch(deleteTodo(todo.id))}>
+                  Delete
+                </button>
+              </>
+            ) : (
+              <>
+                {todo.title}
+                <button onClick={() => setEditTodo(todo)}>Edit</button>
+                <button onClick={() => dispatch(deleteTodo(todo.id))}>
+                  Delete
+                </button>
+              </>
+            )}
+          </li>
+        ))}
+      </ol>
+    </div>
   );
 };
 
@@ -78,7 +102,8 @@ const todosReducer = (state = [], action) => {
     case "UPDATE_TODO":
       const { id, title } = action.payload;
       return state.map((todo) => (todo.id === id ? { ...todo, title } : todo));
-
+    case "ADD_TODOS":
+      return [action.payload, ...state];
     default:
       return state;
   }
